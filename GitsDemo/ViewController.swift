a38487dd444ad94ad35e5f0f35d78d88c35b9b76
emitech990 <emitech990@emitech990s.local>
2019-03-05 16:25:33 +0530

    Changes in Master file

--- GitsDemo/ViewController.swift
+++ GitsDemo/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+     let message = "Hello Git!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       print(message)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
