1635a1b26958ee68aaa8ddf981b30e514901969b
emitech990 <emitech990@emitech990s.local>
2019-03-06 11:34:17 +0530

    test Second User

--- GitsDemo/BViewController.swift
+++ GitsDemo/BViewController.swift
@@ -12,7 +12,7 @@ class BViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("B File")
+        print("B File Second")
         // Do any additional setup after loading the view.
     }
     
